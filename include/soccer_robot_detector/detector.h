c9e69414091776019aca5278388ca27ba895eb8f
王湘鈜 <91529566+d0h0s@users.noreply.github.com>
2025-10-24 11:51:34 +0800

    Implement arc-based ROI detection for soccer robot

--- include/soccer_robot_detector/detector.h
+++ include/soccer_robot_detector/detector.h
@@ -3,11 +3,11 @@
 //
 
 #pragma once
+#include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
-#include <image_transport/image_transport.h>
 
 class SoccerRobotDetector
 {
@@ -16,6 +16,20 @@ public:
   void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 
 private:
+  struct ArcSegment
+  {
+    cv::Point2f center;
+    float radius;
+    std::vector<cv::Point2f> points;
+    cv::Rect bounding_box;
+    double coverage_deg = 0.0;
+    double mean_error = 0.0;
+  };
+
+  bool detectArcSegment(const cv::Mat& gray, ArcSegment& best_arc) const;
+  static double computeAngularCoverage(const std::vector<cv::Point2f>& points,
+                                       const cv::Point2f& center);
+
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
@@ -40,5 +54,10 @@ private:
   int last_radius_ = 0;
   bool has_last_circle_ = false;
 
+  // Arc detection parameters
+  int min_arc_points_;
+  double arc_max_fit_error_;
+  double arc_min_coverage_deg_;
+  double roi_padding_scale_;
 };
 
